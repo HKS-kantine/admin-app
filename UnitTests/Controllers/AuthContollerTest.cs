8c39285b5dc011f2dc9bb936b45e562cbfec75ba
Maarten Jakobs <maarten.jakobs@gmail.com>
2021-01-15 16:50:04 +0100

    added tests

---
+++ UnitTests/Controllers/AuthContollerTest.cs
@@ -0,0 +1,55 @@
+﻿using AdminApp.Controllers;
+using AdminLogic.Entities;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace UnitTests.Controllers
+{
+    public class AuthContollerTest
+    {
+        private readonly AuthController _auth = new AuthController();
+        private LoginEntity _login;
+
+        [SetUp]
+        public void Setup()
+        {
+            _login = new LoginEntity()
+            {
+                Username = "maarten.jakobs@gmail.com",
+                Password = "sonu@123"
+            };
+        }
+
+        [Test]
+        public void AA_AC_01__auth_wrong_username()
+        {
+            _login.Username = null;
+            if (_auth.Authenticate(_login) is OkObjectResult)
+            {
+                Assert.Fail("was authenticated");
+            }
+            Assert.Pass();
+        }
+
+        [Test]
+        public void AA_AC_02__auth_wrong_password()
+        {
+            _login.Password = null;
+            if (_auth.Authenticate(_login) is OkObjectResult)
+            {
+                Assert.Fail("was authenticated");
+            }
+            Assert.Pass();
+        }
+
+        [Test]
+        public void AA_AC_03__auth_loggedin()
+        {
+            if (_auth.Authenticate(_login) is OkObjectResult)
+            {
+                Assert.Pass();
+            }
+            Assert.Fail("was not authenticated");
+        }
+    }
+}
---
+++ UnitTests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using AdminApp.Controllers;
+using AdminLogic.Entities;
+using AdminProduct;
+using NUnit.Framework;
+
+namespace UnitTests.Controllers
+{
+    public class ProductControllerTest
+    {
+        private readonly AdminApp.Controllers.ProductController _product = new AdminApp.Controllers.ProductController();
+        private List<ProductEntity> _productList;
+        private ProductEntity _newProduct;
+        private ProductEntity _lastAddedProduct;
+
+        [SetUp]
+        public void Setup()
+        {
+            _newProduct = new ProductEntity()
+            {
+                Name = "Test-edit",
+                Price = 1,
+                Image = "MAARTEN.jpg"
+            };
+
+            _productList = _product.Get();
+            _lastAddedProduct = _productList[_productList.Count - 1];
+            _lastAddedProduct.Name = "Test-edit";
+            _lastAddedProduct.Price = 2;
+            _lastAddedProduct.Image = "TEST.jpg";
+        }
+
+        [Test]
+        public void AA_PR_01__receive_list_of_products()
+        {
+            _productList = _product.Get();
+            if (_productList.Count >= 1)
+            {
+                Assert.Pass();
+            }
+
+            Assert.Fail("List is empty");
+        }
+
+        [Test]
+        public void AA_PR_02__create_new_product_without_image()
+        {
+            _newProduct.Image = null;
+            _product.Post(_newProduct);
+            _productList = _product.Get();
+            isSameProduct(_productList[_productList.Count - 1], _newProduct);
+        }
+
+        [Test]
+        public void AA_PR_03__create_new_product_without_name()
+        {
+            _newProduct.Name = null;
+            _product.Post(_newProduct);
+            _productList = _product.Get();
+            isSameProduct(_productList[_productList.Count - 1], _newProduct);
+        }
+
+        [Test]
+        public void AA_PR_04__create_new_product_without_price()
+        {
+            _newProduct.Price = 0;
+            _product.Post(_newProduct);
+            _productList = _product.Get();
+            isSameProduct(_productList[_productList.Count - 1], _newProduct);
+        }
+
+        [Test]
+        public void AA_PR_05__create_new_product()
+        {
+            _product.Post(_newProduct);
+            _productList = _product.Get();
+            isSameProduct(_productList[_productList.Count - 1], _newProduct);
+        }
+
+        [Test]
+        public void AA_PR_06_edit_product_without_image()
+        {
+            _lastAddedProduct.Image = null;
+            _product.Post(_lastAddedProduct);
+            isSameProduct(_productList[_productList.Count - 1], _lastAddedProduct);
+        }
+
+        [Test]
+        public void AA_PR_07_edit_product_without_name()
+        {
+            _lastAddedProduct.Name = null;
+            _product.Post(_lastAddedProduct);
+            isSameProduct(_productList[_productList.Count - 1], _lastAddedProduct);
+        }
+
+        [Test]
+        public void AA_PR_08_edit_product_without_price()
+        {
+            _lastAddedProduct.Price = 0;
+            _product.Post(_lastAddedProduct);
+            isSameProduct(_productList[_productList.Count - 1], _lastAddedProduct);
+        }
+
+        [Test]
+        public void AA_PR_09_edit_product()
+        {
+            _product.Post(_lastAddedProduct);
+            isSameProduct(_productList[_productList.Count - 1], _lastAddedProduct);
+        }
+
+        private void isSameProduct(ProductEntity returnProduct, ProductEntity givenProduct)
+        {
+            if (returnProduct.Name != givenProduct.Name)
+            {
+                Assert.Fail("not same Name");
+            }
+            if (returnProduct.Price != givenProduct.Price)
+            {
+                Assert.Fail("not same Price");
+            }
+            if (returnProduct.Image != givenProduct.Image)
+            {
+                Assert.Fail("not same Image");
+            }
+            Assert.Pass();
+        }
+    }
+}
